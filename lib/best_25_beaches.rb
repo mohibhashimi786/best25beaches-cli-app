9b1b0cdc27d4e8d195edf576960857cd1d820c93
Ahmad <github email address>
2018-02-18 17:11:50 -0800

    stub layout of cli

--- lib/best_25_beaches.rb
+++ lib/best_25_beaches.rb
@@ -1,2 +1,7 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+
 require_relative "./best_25_beaches/version"
-require_relative './best_25_beaches/cli'
+require_relative './best_25_beaches/cli' 
--- lib/best_25_beaches/cli.rb
+++ lib/best_25_beaches/cli.rb
@@ -3,7 +3,21 @@ class Best25Beaches::CLI
 
 	def call
 		puts "top beaches"
+		menu
+		exit
 	end
 
+	
+	def menu
+
+		
+
+	end
+
+
+
+	def exit
+		"Please come backs soon"
+	end
 
 end
