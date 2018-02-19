132da5238854ab2e80431a1635e2dfac84a36da2
Ahmad <github email address>
2018-02-18 21:02:13 -0800

    finish adding attributes and respective xpaths

--- lib/best_25_beaches/beach.rb
+++ lib/best_25_beaches/beach.rb
@@ -26,22 +26,24 @@ class Best25Beaches::Beach
 	end
 
 	def webpage
-		webpage ||= Nokogiri::HTML(open(self.url))
+		@webpage ||= Nokogiri::HTML(open(self.url))
 	end
 
 
 	def activities
-		activities ||= webpage.xpath("//*[@id="taplc_location_detail_header_attractions_0"]/div[1]/span[3]/div")
+		@activities ||= webpage.xpath("//*[@id='taplc_location_detail_header_attractions_0']/div[1]/span[3]/div")
 	end
 
 	def rating
+		@rating ||= webpage.xpath("//*[@id='taplc_location_detail_overview_attraction_0']/div[1]/div[2]/div[2]/div[2]/div[1]/span")
 	end
 
 	def excellent_review_percentage
+		excellent_review_percentage ||= webpage.xpath("||= //*[@id='taplc_location_detail_overview_attraction_0']/div[1]/div[2]/div[2]/div[2]/div[2]/ul/li[1]/span[3]")
 	end
 
 	def visitor_thoughts
-
+		@visitor_thoughts ||= webpage.xpath("//*[@id='taplc_location_detail_overview_attraction_0']/div[1]/div[2]/div[2]/div[1]/div[1]/div/div[1]/text()")
 	end
 
 
