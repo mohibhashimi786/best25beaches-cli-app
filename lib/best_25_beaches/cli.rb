3bbe8331aaac2f4928731b23947ccdbe2d360c38
Ahmad <github email address>
2018-02-22 22:19:26 -0800

    continue stub and code user interaction

--- lib/best_25_beaches/cli.rb
+++ lib/best_25_beaches/cli.rb
@@ -1,29 +1,28 @@
 #CLI controller
 class Best25Beaches::CLI
 
-	
-
-
 	def call
 	Best25Beaches::Scraper.run
 	menu
-		
+	exit
 	end
 
-
-	
 	def menu
-		puts "Welcome to the first step of your dream destination vacation!"
-		puts "Here are the top 25 best beach destinations in the world:"
-		Best25Beaches::Beach.all.each.with_index do |a,b|
-		puts "#{b+1}. #{a.name}"
-		end	
+		input = ""
+			while input != 'exit'
+			puts "Welcome to the first step of your dream destination vacation!"
+			puts "Here are the top 25 best beach destinations in the world:"
+				Best25Beaches::Beach.all.each.with_index do |a,b|
+					puts "			#{b+1}. #{a.name}"
+				end
+			puts "Please enter the number of the beach you would like  more information on or type 'exit' to leave the program."
+			input = gets.strip
+			end
+			Best25Beaches::Beach.locate(input.to_i)	
 	end
-
-
 
 	def exit
-		"Please come backs soon"
+		puts "Please come backs soon, Goodbye :-)"
 	end
 
 end
--- lib/best_25_beaches/scraper.rb
+++ lib/best_25_beaches/scraper.rb
@@ -33,29 +33,28 @@ class Best25Beaches::Scraper
 	end
 
 	def self.webpage_scrape
-			Best25Beaches::Beach.all.each do |beach|
-				webpage = Nokogiri::HTML(open(beach.url))
-				beach.excellent_review_percentage ||= webpage.css("span.row_count.row_cell").text[0...3]
-				beach.rating ||= webpage.css("#taplc_location_detail_overview_attraction_0 > div.block_wrap.easyClear > div.overviewContent > div.ui_columns.is-multiline.is-mobile.reviewsAndDetails > div.ui_column.is-6.reviews > div.rating > span").text
-				beach.activities ||= webpage.xpath("//*[@id='taplc_location_detail_header_attractions_0']/div[1]/span[3]/div").css("a").text
+		Best25Beaches::Beach.all.each do |beach|
+			webpage = Nokogiri::HTML(open(beach.url))
+			beach.excellent_review_percentage ||= webpage.css("span.row_count.row_cell").text[0...3]
+			beach.rating ||= webpage.css("#taplc_location_detail_overview_attraction_0 > div.block_wrap.easyClear > div.overviewContent > div.ui_columns.is-multiline.is-mobile.reviewsAndDetails > div.ui_column.is-6.reviews > div.rating > span").text
+			beach.activities ||= webpage.xpath("//*[@id='taplc_location_detail_header_attractions_0']/div[1]/span[3]/div").css("a").text
 	 	end
 	end
 
-
 	def self.nearby
-			Best25Beaches::Beach.all.each do |beach|
-				webpage = Nokogiri::HTML(open(beach.url))
-				beach.hotels = []
-				beach.restaurants = []
-				beach.nearby_attractions = []
-				counter = 1
-					while counter <= 3
-					beach.hotels << webpage.xpath("//*[@id='LOCATION_TAB']/div[3]/div/div[#{counter}]/div/div[2]/div[1]").text
-					beach.restaurants << webpage.xpath("//*[@id='LOCATION_TAB']/div[4]/div/div[#{counter}]/div/div[2]/div[1]").text
-					beach.nearby_attractions << webpage.xpath("//*[@id='LOCATION_TAB']/div[5]/div/div[#{counter}]/div/div[2]/div[1]").text
-					counter += 1
-					end
-			end
+		Best25Beaches::Beach.all.each do |beach|
+			webpage = Nokogiri::HTML(open(beach.url))
+			beach.hotels = []
+			beach.restaurants = []
+			beach.nearby_attractions = []
+			counter = 1
+				while counter <= 3
+				beach.hotels << webpage.xpath("//*[@id='LOCATION_TAB']/div[3]/div/div[#{counter}]/div/div[2]/div[1]").text
+				beach.restaurants << webpage.xpath("//*[@id='LOCATION_TAB']/div[4]/div/div[#{counter}]/div/div[2]/div[1]").text
+				beach.nearby_attractions << webpage.xpath("//*[@id='LOCATION_TAB']/div[5]/div/div[#{counter}]/div/div[2]/div[1]").text
+				counter += 1
+				end
+		end
 	end
 
 end
