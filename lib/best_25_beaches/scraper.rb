0caee9d156b8e9d6fe2f35a52e7c0f95896ccc2d
Ahmad <github email address>
2018-02-22 19:32:29 -0800

    remove 'visitor_thoughts' attribute majority  of the beaches has this field as empty.

--- lib/best_25_beaches/scraper.rb
+++ lib/best_25_beaches/scraper.rb
@@ -35,7 +35,6 @@ class Best25Beaches::Scraper
 	def self.webpage_scrape
 			Best25Beaches::Beach.all.each do |beach|
 			webpage = Nokogiri::HTML(open(beach.url))
-			beach.visitor_thoughts ||= webpage.css("#taplc_location_detail_overview_attraction_0 > div.block_wrap.easyClear > div.overviewContent > div.ui_columns.is-multiline.is-mobile.reviewsAndDetails > div.ui_column.is-12.descriptionRow > div.prw_rup.prw_common_location_description > div > div.text").text
 			beach.excellent_review_percentage ||= webpage.css("span.row_count.row_cell").text[0...3]
 			beach.rating ||= webpage.css("#taplc_location_detail_overview_attraction_0 > div.block_wrap.easyClear > div.overviewContent > div.ui_columns.is-multiline.is-mobile.reviewsAndDetails > div.ui_column.is-6.reviews > div.rating > span").text
 			beach.activities ||= webpage.xpath("//*[@id='taplc_location_detail_header_attractions_0']/div[1]/span[3]/div").css("a").text
