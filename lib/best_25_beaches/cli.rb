4c74a325b1a7708a9c71102594a55cfe24a06556
Ahmad <github email address>
2018-02-23 10:00:49 -0800

    continue to s
    tub user interaction

--- lib/best_25_beaches/cli.rb
+++ lib/best_25_beaches/cli.rb
@@ -7,18 +7,44 @@ class Best25Beaches::CLI
 	exit
 	end
 
+	def list
+		Best25Beaches::Beach.all.each.with_index do |a,b|
+			puts "			#{b+1}. #{a.name}"
+		end
+	end
+
+
 	def menu
-		input = ""
-			while input != 'exit'
+		
 			puts "Welcome to the first step of your dream destination vacation!"
 			puts "Here are the top 25 best beach destinations in the world:"
-				Best25Beaches::Beach.all.each.with_index do |a,b|
-					puts "			#{b+1}. #{a.name}"
+			list
+			input = nil
+			until input == 'exit'
+			puts "Please enter the number of the beach you would like more information on or type 'exit' to leave the program."
+			
+				input = gets.strip
+				if input == 'list'
+					list
+				elsif input.to_i > 0
+
+					if beach = Best25Beaches::Beach.locate(input.to_i)
+					print_beach(beach)
+						input1 = nil
+						until input1 == 'exit' || input1 == 'hotels' || input1 == 'restaurants' || input1 == 'attractions' || input1 == 'menu'
+						puts "For nearby hotels, please enter 'hotels, for nearby restaurants, please 'restaurants, and for nearby attractions, please type 'attractions."
+						puts "If you would like information on another beach, please type 'menu' or type 'exit' to leave the program."
+					end
+					end
 				end
-			puts "Please enter the number of the beach you would like  more information on or type 'exit' to leave the program."
-			input = gets.strip
+
 			end
-			Best25Beaches::Beach.locate(input.to_i)	
+	end
+
+	def print_beach(beach)
+		puts "**********************#{beach.name}**********************"
+		puts "**********************#{beach.location}**********************"
+		puts "***********#{beach.description}***********"
 	end
 
 	def exit
