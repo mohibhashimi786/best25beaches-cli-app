357de17d39ec6f99dca2efec29439e8cf33becd5
Ahmad <github email address>
2018-02-25 21:39:27 -0800

    test loops of the menu to ensure weed out breaks

--- lib/best_25_beaches/cli.rb
+++ lib/best_25_beaches/cli.rb
@@ -17,22 +17,22 @@ class Best25Beaches::CLI
 
 
 	def menu
-		
-			
 			list
 			puts "Please enter the number of the beach you would like more information on or type 'exit' to leave the program."
 			input = nil
-			
 				input = gets.strip
-				if input == 'list'
-					list
-				elsif input.to_i > 0
-					beach = Best25Beaches::Beach.locate(input.to_i)
-					print_beach(beach)
-				else input == 'exit'
+				if input == 'exit'
 					exit
+				elsif input.to_i.between?(1, 26)
+					if beach = Best25Beaches::Beach.locate(input.to_i)
+					print_beach(beach)
+					puts "If you would like information on another beach, please type 'menu' to choose again or type 'exit' to leave the program."
+					else
+						menu
+					end
+				else
+					menu
 				end
-
 	end
 
 	def print_beach(beach)
@@ -41,8 +41,14 @@ class Best25Beaches::CLI
 			puts "NAME:  #{beach.name.upcase}".center(50, "-")
 			puts "LOCATION:  #{beach.location}".center(50, "-")
 			puts "DESCRIPTION:  #{beach.description}".center(50, "-")
+			puts "BEACH RATING: #{beach.rating}".center(50, "-")
+			puts "BEACH ACTIVITIES: #{beach.activities}".center(50, "-")
+			puts "EXCELLENT REVIEW PERCENTAGE:  #{beach.excellent_review_percentage}".center(50, "-")
+			puts ""
+			puts ""
 			puts "For nearby hotels, please enter 'hotels, for nearby restaurants, please 'restaurants, and for nearby attractions, please type 'attractions."
 			puts "If you would like information on another beach, please type 'menu' or type 'exit' to leave the program."
+			puts ""
 			input1 = gets.strip
 				case input1
 					when "exit"
@@ -58,11 +64,12 @@ class Best25Beaches::CLI
 						 	puts "#{index}. #{restaurant}"
 						    end
 					when "attractions"
-						beach.attractions.each.with_index(1) do |attraction, index|
+						beach.nearby_attractions.each.with_index(1) do |attraction, index|
 							puts "#{index}. #{attraction}"
 							end
 				end
 		end
+		
 	end
 
 	def exit
