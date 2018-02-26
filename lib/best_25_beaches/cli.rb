1f7e6ec8d5d137194f4a5d61440187cfca480a70
Ahmad <github email address>
2018-02-25 19:56:40 -0800

    continue stubbing and coding cli to correctly output attributes

--- lib/best_25_beaches/cli.rb
+++ lib/best_25_beaches/cli.rb
@@ -1,13 +1,15 @@
 #CLI controller
+require 'colorize'
 class Best25Beaches::CLI
 
 	def call
 	Best25Beaches::Scraper.run
 	menu
-	exit
 	end
 
 	def list
+		puts "Welcome to the first step of your dream destination vacation!"
+			puts "Here are the top 25 best beach destinations in the world:"
 		Best25Beaches::Beach.all.each.with_index do |a,b|
 			puts "			#{b+1}. #{a.name}"
 		end
@@ -16,35 +18,45 @@ class Best25Beaches::CLI
 
 	def menu
 		
-			puts "Welcome to the first step of your dream destination vacation!"
-			puts "Here are the top 25 best beach destinations in the world:"
+			
 			list
-			input = nil
-			until input == 'exit'
 			puts "Please enter the number of the beach you would like more information on or type 'exit' to leave the program."
+			input = nil
 			
 				input = gets.strip
 				if input == 'list'
 					list
 				elsif input.to_i > 0
-
-					if beach = Best25Beaches::Beach.locate(input.to_i)
+					beach = Best25Beaches::Beach.locate(input.to_i)
 					print_beach(beach)
-						input1 = nil
-						until input1 == 'exit' || input1 == 'hotels' || input1 == 'restaurants' || input1 == 'attractions' || input1 == 'menu'
-						puts "For nearby hotels, please enter 'hotels, for nearby restaurants, please 'restaurants, and for nearby attractions, please type 'attractions."
-						puts "If you would like information on another beach, please type 'menu' or type 'exit' to leave the program."
-					end
-					end
+				else input == 'exit'
+					exit
 				end
 
-			end
 	end
 
 	def print_beach(beach)
-		puts "**********************#{beach.name}**********************"
-		puts "**********************#{beach.location}**********************"
-		puts "***********#{beach.description}***********"
+		input1 = ""
+		until input1 == 'exit' || input1 == 'hotels' || input1 == 'restaurants' || input1 == 'attractions' || input1 == 'menu'
+			puts "**********************#{beach.name}**********************"
+			puts "**********************#{beach.location}**********************"
+			puts "***********#{beach.description}***********"
+			puts "For nearby hotels, please enter 'hotels, for nearby restaurants, please 'restaurants, and for nearby attractions, please type 'attractions."
+			puts "If you would like information on another beach, please type 'menu' or type 'exit' to leave the program."
+			input1 = gets.strip
+				case input1
+					when "exit"
+						exit
+					when 'menu'
+						menu
+					when "hotels"
+						puts "#{beach.hotels}".center(50, '_').colorize(:white)
+					when "restaurants"
+						puts "#{beach.restaurants}".center(50, '_').colorize(:light_blue)
+					when "attractions"
+						puts "#{beach.attractions}".center(50, '_').colorize(:light_blue)
+				end
+		end
 	end
 
 	def exit
